2f81d14c6b099f29fb211395696f82f1003f0be1
Monica Luquerna <monica_luquerna@outlook.com>
2024-10-01 22:33:32 -0500

    Inicio del proyecto portafolio

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+document.addEventListener("DOMContentLoaded", function() {
+    const preloader = document.getElementById('preloader');
+    window.onload = function() {
+        preloader.style.display = 'none';
+    }
+});
