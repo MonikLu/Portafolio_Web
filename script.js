23c468fb85526323f77118578398757ba937f296
Monica Luquerna <98406035+MonikLu@users.noreply.github.com>
2024-10-03 18:14:26 -0500

    validador formulario

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+//Preloader
 document.addEventListener("DOMContentLoaded", function() {
     const preloader = document.getElementById('preloader');
     window.onload = function() {
         preloader.style.display = 'none';
     }
 });
+
+document.querySelector(".contacto__formulario").addEventListener("submit", function(event) {
+    const nombre = document.querySelector(".contacto__campo[type='text']").value.trim();
+    const correo = document.querySelector(".contacto__campo[type='email']").value.trim();
+    const asunto = document.querySelector(".contacto__campo[type='tema']").value.trim();
+    const mensaje = document.querySelector(".contacto__campo--textarea").value.trim();
+    
+    if (nombre === "" || correo === "" || asunto === "" || mensaje === ""){
+        alert("Por favor, rellena todos los campos");
+        event.preventDefault();
+        return;
+    }
+
+    if (!validateEmail(correo)) {
+        alert("Por favor, introduce un correo electrónico válido");
+        event.preventDefault();
+        return;
+    }
+
+    alert("¡Mensaje enviado con éxito!");
+});
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
